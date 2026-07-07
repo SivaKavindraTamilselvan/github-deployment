f65a170b177416fad75106f7a13322e242dc3c19
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-07-07 10:46:28 +0530

    Bonus implemented

--- src/app/weather-app/weather-app.ts
+++ src/app/weather-app/weather-app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { WeatherService } from './service';
 import { Weather } from './model';
 
@@ -9,13 +9,28 @@ import { Weather } from './model';
   styleUrl: './weather-app.css',
 })
 export class WeatherApp {
-  private weatherService = inject(WeatherService);
-
+  loading = signal(false);
   weather = signal<Weather[]>([]);
+  error = signal('');
+  totalRecords = computed(() => this.weather()?.length ?? 0);
+
+  constructor(private weatherService: WeatherService) {
+
+  }
 
   ngOnInit() {
-    this.weatherService.getWeather().subscribe(data => {
-      this.weather.set(data);
+    this.loading.set(true);
+    this.error.set('');
+
+    this.weatherService.getWeather().subscribe({
+      next: (response) => {
+        this.weather.set(response);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set('Unable to load weather data. Please try again later.');
+        this.loading.set(false);
+      }
     });
   }
 }
